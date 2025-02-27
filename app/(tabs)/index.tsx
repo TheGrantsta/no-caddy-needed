fef01af62910f96887a733bcbc12a9a5f2532ce7
Grant McKenna <grant.mckenna@madetech.com>
2025-02-27 09:10:34 +0000

    Amend homepage to include refresh control

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,30 @@
-import { ScrollView, Text, View } from 'react-native';
+import { useState } from 'react';
+import { RefreshControl, ScrollView, Text, View } from 'react-native';
 import ScreenWrapper from '../screen-wrapper';
-import styles from '@/assets/stlyes';
 import Chrevons from '@/components/Chevrons';
+import styles from '@/assets/stlyes';
+import colours from '@/assets/colours';
 
 export default function HomeScreen() {
   const points = ['Control low point', 'Improve centre strike', 'Enhance clubface control'];
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 750);
+  };
 
   return (
     <ScreenWrapper>
-      <ScrollView>
+      <ScrollView style={styles.scrollContainer} refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          tintColor={colours.yellow} />
+      }>
         <View style={styles.viewContainer}>
           <Text style={[styles.subHeaderText, { marginBottom: 10 }]}>
             Golfers seeking smarter practice & setting better on course expectations

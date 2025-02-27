4a554e8f1ea00fb7475f9ed94d26ef7151f15d13
Grant McKenna <grant.mckenna@madetech.com>
2025-02-27 09:08:17 +0000

    Amend homepage to show heading and related points

--- __tests__/app/homepage-test.tsx
+++ __tests__/app/homepage-test.tsx
@@ -8,4 +8,15 @@ describe('renders homepage', () => {
 
         expect(getByText('Golfers seeking smarter practice & setting better on course expectations')).toBeTruthy();
     });
+
+    it('shows heading and points', () => {
+        const points = ['Control low point', 'Improve centre strike', 'Enhance clubface control'];
+        const { getByText } = render(<Homepage />);
+
+        expect(getByText('Get the ball in the hole in the fewest shots')).toBeTruthy();
+        expect(getByText('Control low point')).toBeTruthy();
+        expect(getByText('Improve centre strike')).toBeTruthy();
+        expect(getByText('Enhance clubface control')).toBeTruthy();
+    });
 });
+
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
 import { ScrollView, Text, View } from 'react-native';
 import ScreenWrapper from '../screen-wrapper';
 import styles from '@/assets/stlyes';
+import Chrevons from '@/components/Chevrons';
 
 export default function HomeScreen() {
+  const points = ['Control low point', 'Improve centre strike', 'Enhance clubface control'];
+
   return (
     <ScreenWrapper>
       <ScrollView>
@@ -10,6 +13,8 @@ export default function HomeScreen() {
           <Text style={[styles.subHeaderText, { marginBottom: 10 }]}>
             Golfers seeking smarter practice & setting better on course expectations
           </Text>
+
+          <Chrevons heading='Get the ball in the hole in the fewest shots' points={points} />
         </View>
       </ScrollView>
     </ScreenWrapper>

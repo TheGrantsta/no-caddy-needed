6e25b8de0731b0ec6967771311698dbc3753a1b4
Grant McKenna <grant.mckenna@madetech.com>
2025-03-11 21:51:54 +0000

    Amend project to show practice page

---
+++ __tests__/app/practice-test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import View from '../../app/(tabs)/practice';
+
+jest.mock('react-native-gesture-handler', () => {
+    const GestureHandler = jest.requireActual('react-native-gesture-handler');
+    return {
+        ...GestureHandler,
+        GestureHandlerRootView: jest
+            .fn()
+            .mockImplementation(({ children }) => children),
+    };
+});
+
+describe('Practice page ', () => {
+    it('renders correctly with the default text', () => {
+        const { getByText } = render(<View />);
+
+        expect(getByText('Practice')).toBeTruthy();
+        expect(getByText('Make your practice time more effective')).toBeTruthy();
+    });
+});

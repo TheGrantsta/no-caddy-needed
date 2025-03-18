559ed7376ea321ebfe6d45c40d341773c33a3ac7
Grant McKenna <grant.mckenna@madetech.com>
2025-03-18 09:03:54 +0000

    Add settings test

---
+++ __tests__/app/settings-test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import View from '../../app/(tabs)/settings';
+import { getWedgeChartService } from '../../service/DbService';
+
+jest.mock('../../service/DbService', () => ({
+    getWedgeChartService: jest.fn(() => [[]])
+}));
+
+jest.mock('react-native-gesture-handler', () => {
+    const GestureHandler = jest.requireActual('react-native-gesture-handler');
+    return {
+        ...GestureHandler,
+        GestureHandlerRootView: jest
+            .fn()
+            .mockImplementation(({ children }) => children),
+    };
+});
+
+describe('Settings page ', () => {
+    it('renders page title', () => {
+        const { getByText } = render(<View />);
+
+        expect(getByText('Settings')).toBeTruthy();
+    });
+
+    it('renders heading', () => {
+        const { getByText } = render(<View />)
+
+        expect(getByText('Wedge chart')).toBeTruthy();
+    });
+});

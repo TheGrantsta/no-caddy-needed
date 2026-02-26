f33b1fc0af30ce63d56a99ed7f93f4bab18a03d1
Grant McKenna <grant.mckenna@madetech.com>
2026-02-26 09:51:14 +0000

    Amend app icon for ios

---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dependencies: {
+    '@react-native-firebase/app': {
+      platforms: { ios: null },
+    },
+    '@react-native-firebase/analytics': {
+      platforms: { ios: null },
+    },
+    '@react-native-firebase/crashlytics': {
+      platforms: { ios: null },
+    },
+  },
+};

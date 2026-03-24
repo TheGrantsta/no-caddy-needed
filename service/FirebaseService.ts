366b5a05a1cac8d8dd7d3bc480610f30acd605d8
Grant McKenna <grant.mckenna@madetech.com>
2026-03-24 11:50:48 +0000

    Amend firebase services

--- service/FirebaseService.ts
+++ service/FirebaseService.ts
@@ -3,11 +3,8 @@ import { getFirestore, addDoc, collection, serverTimestamp } from 'firebase/fire
 
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);

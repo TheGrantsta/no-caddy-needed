e8562bc160939a16aa546ba7d813d59ddc04dda9
Grant McKenna <grant.mckenna@madetech.com>
2026-04-04 22:28:23 +0100

    Amend project to tidy up unused photos

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,21 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import defaultStyles from '@/assets/styles';
-
 export default function NotFoundScreen() {
-  const styles = defaultStyles.notFound;
-
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+    return (
+        <>
+            <Stack.Screen options={{ title: 'Oops!' }} />
+            <View style={styles.container}>
+                <Text style={styles.title}>This screen doesn't exist.</Text>
+                <Link href="/" style={styles.link}>
+                    <Text>Go to home screen!</Text>
+                </Link>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+});
